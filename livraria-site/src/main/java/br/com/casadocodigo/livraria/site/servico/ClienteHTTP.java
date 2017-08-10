93b24a1c67ae9df26d29dc384b724fe958a3772d
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-10 16:56:09 -0300

    adicionado repasse de exceção

--- livraria-site/src/main/java/br/com/casadocodigo/livraria/site/servico/ClienteHTTP.java
+++ livraria-site/src/main/java/br/com/casadocodigo/livraria/site/servico/ClienteHTTP.java
@@ -1,7 +1,9 @@
 package br.com.casadocodigo.livraria.site.servico;
 
+import br.com.casadocodigo.livraria.site.excecoes.ServidorIndisponivelException;
+
 public interface ClienteHTTP {
 	
-	String get(String url);
+	String get(String url) throws ServidorIndisponivelException;
 
 }
