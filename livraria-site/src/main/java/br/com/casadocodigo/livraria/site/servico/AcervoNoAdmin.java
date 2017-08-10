302244ec265df2983b872ba5ee88532ad03493c1
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-10 17:02:27 -0300

    implementado método que busca o xml no admin

--- livraria-site/src/main/java/br/com/casadocodigo/livraria/site/servico/AcervoNoAdmin.java
+++ livraria-site/src/main/java/br/com/casadocodigo/livraria/site/servico/AcervoNoAdmin.java
@@ -9,9 +9,16 @@ import br.com.casadocodigo.livraria.modelo.Livro;
 @Component
 public class AcervoNoAdmin implements Acervo {
 
+	private ClienteHTTP http;
+
+	public AcervoNoAdmin(ClienteHTTP http) {
+		this.http = http;
+	}
+
 	@Override
 	public List<Livro> todosOsLivros() {
-		// TODO Auto-generated method stub
+		String url = "http://localhost:8080/livraria-admin" + "/integracao/listaLivros";
+		String resposta = http.get(url);
 		return null;
 	}
 
