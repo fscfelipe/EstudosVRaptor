6e759a851e0ba3a83d6fc447c1da26727597860e
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-10 16:33:29 -0300

    é necessário ter o modelo do livro no cliente

--- livraria-site/src/main/java/br/com/casadocodigo/livraria/dao/controlador/Acervo.java
+++ livraria-site/src/main/java/br/com/casadocodigo/livraria/dao/controlador/Acervo.java
@@ -1,5 +1,9 @@
 package br.com.casadocodigo.livraria.dao.controlador;
 
+import java.util.List;
+
+import br.com.casadocodigo.livraria.modelo.Livro;
+
 public interface Acervo {
 	
 	List<Livro> todosOsLivros();
