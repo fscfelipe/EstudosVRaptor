0b8f73ef72f4c18db7ca6961a80a55eee08578ae
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-11 17:19:21 -0300

    interceptor de autenticação que irá funcionar no LoginController

---
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/interceptors/AutenticacaoInterceptor.java
@@ -0,0 +1,44 @@
+package br.com.casadocodigo.livraria.interceptors;
+
+import br.com.caelum.vraptor.InterceptionException;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.core.InterceptorStack;
+import br.com.caelum.vraptor.interceptor.Interceptor;
+import br.com.caelum.vraptor.resource.ResourceMethod;
+import br.com.casadocodigo.livraria.controlador.LoginController;
+import br.com.casadocodigo.livraria.modelo.UsuarioLogado;
+
+public class AutenticacaoInterceptor implements Interceptor {
+	
+	// Ver pag.134
+	
+	private UsuarioLogado usuario;
+	private Result result;
+	
+	public AutenticacaoInterceptor(UsuarioLogado usuario, Result result) {
+		this.usuario = usuario;
+		this.result = result;
+	}
+
+	@Override
+	public boolean accepts(ResourceMethod method) {
+	
+		return !method.getResource().getType().equals(LoginController.class);
+	}
+
+	@Override
+	public void intercept(
+			InterceptorStack stack, 
+			ResourceMethod method, 
+			Object controller) throws InterceptionException {
+		
+		if(usuario.isLogado()) {
+			stack.next(method, controller);
+		}else {
+			result.redirectTo(LoginController.class).formulario();
+		}
+		
+
+	}
+
+}
