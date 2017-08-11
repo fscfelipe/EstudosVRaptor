cd37174bb1f91d16c70756cf7453c1f88643d4f8
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-11 14:50:23 -0300

    implementado exemplo para fazer um log dos requests

--- livraria-admin/src/main/java/br/com/casadocodigo/livraria/interceptors/TransacoesInterceptor.java
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/interceptors/TransacoesInterceptor.java
@@ -1,16 +1,25 @@
 package br.com.casadocodigo.livraria.interceptors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import br.com.caelum.vraptor.InterceptionException;
 import br.com.caelum.vraptor.Intercepts;
 import br.com.caelum.vraptor.core.InterceptorStack;
 import br.com.caelum.vraptor.interceptor.Interceptor;
+import br.com.caelum.vraptor.ioc.RequestScoped;
 import br.com.caelum.vraptor.resource.ResourceMethod;
 
 @Intercepts
+@RequestScoped
 public class TransacoesInterceptor implements Interceptor {
+	
+	//O exemplo a seguir é somente para teste e ilustração.
+	//Ele faz um log dos métodos que forem chamados pela requisição
+	
+	private final HttpServletRequest request;
 
-	public TransacoesInterceptor() {
-		
+	public TransacoesInterceptor(HttpServletRequest request) {
+		this.request = request;
 	}
 	
 	@Override
@@ -26,6 +35,13 @@ public class TransacoesInterceptor implements Interceptor {
 		
 		// O livro utiliza JPA para inplementar a interceptação da transação
 		// ver pag. 124
+		
+		System.out.println("Interceptando " + request.getRequestURI());
+        // código a ser executado antes da lógica
+
+        stack.next(method, controller); // continua a execução
+
+        // código a ser executádo depois da lógica
 
 	}
 
