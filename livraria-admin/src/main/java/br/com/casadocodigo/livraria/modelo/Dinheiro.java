0c69839705885ca79316ac989b3b84f90e4be846
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-13 01:17:18 -0300

    modelo para dinheiro

---
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/modelo/Dinheiro.java
@@ -0,0 +1,10 @@
+package br.com.casadocodigo.livraria.modelo;
+
+import java.math.BigDecimal;
+
+import br.com.casadocodigo.livraria.utils.Moeda;
+
+public class Dinheiro {
+	private Moeda moeda;
+	private BigDecimal montante;
+}
