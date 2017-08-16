cd27d7be498e798563c1eab40c3da52937ac23ae
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-15 23:57:37 -0300

    anotação para permitir método de integração de sistema

---
+++ livraria-admin/src/main/java/br/com/casadocodigo/livraria/anotacoes/Integracao.java
@@ -0,0 +1,12 @@
+package br.com.casadocodigo.livraria.anotacoes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {ElementType.METHOD})
+public @interface Integracao {
+
+}
